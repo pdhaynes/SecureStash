a7d0f455626aa80911918a8d1ba8887d27c5f723
Peyton <haynesp2001@gmail.com>
2025-01-02 14:40:08 -0600

    Removed unnecessary import.

--- app/src/main/java/com/example/securestash/Adapters/DirectoryAdapter.kt
+++ app/src/main/java/com/example/securestash/Adapters/DirectoryAdapter.kt
@@ -5,7 +5,6 @@ import android.content.res.ColorStateList
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.PorterDuff
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
